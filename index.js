734b525e6f115569864e98c4c4f33340fd64dfb8
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-25 17:51:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(icecream) {
+  debugger;
+  return `I love ${icecream}`;
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
